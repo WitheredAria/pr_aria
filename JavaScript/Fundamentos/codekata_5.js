2261f6c4cc89d6a5a853ccd7bd80992b1259f1b1
WitheredAria <55595867+WitheredAria@users.noreply.github.com>
2026-03-18 15:15:27 +0000

    codekata 5, 6

---
+++ JavaScript/Fundamentos/codekata_5.js
@@ -0,0 +1,9 @@
+let hasPizza = "No hay."
+
+if (hasPizza == false) {
+    console.log("No tengo pizza.");
+} else if (hasPizza == true) {
+    console.log("Tengo pizza.");
+} else {
+    console.log(hasPizza);
+}
