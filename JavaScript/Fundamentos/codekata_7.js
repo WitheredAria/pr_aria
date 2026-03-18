99449673728809d8081384482716ec4d55af31d2
WitheredAria <55595867+WitheredAria@users.noreply.github.com>
2026-03-18 15:32:35 +0000

    codekata 7

---
+++ JavaScript/Fundamentos/codekata_7.js
@@ -0,0 +1,18 @@
+/* function printName() {
+    let name = "WitheredAria";
+    let surname = "Hernández Negrín";
+
+    if (name == "WitheredAria") {
+        console.log(surname);
+    }
+};
+
+printName(); */
+
+let name = "WitheredAria";
+
+if (true) {
+    let text = "Texto de ejemplo."
+};
+
+console.log(text);
