94a1c4805927d4059b96b34fa88f37e6acb58605
WitheredAria <55595867+WitheredAria@users.noreply.github.com>
2026-03-18 16:16:28 +0000

    codekata 9

---
+++ JavaScript/Fundamentos/codekata_9.js
@@ -0,0 +1,14 @@
+window.console.log("Hola.");
+window.alert("Hola.");
+window.print();
+window.scrollTo(0,0);
+document.querySelector(".o3j99.n1xJcf.Ne6nSd");
+document.querySelector(".o3j99.n1xJcf.Ne6nSd").remove;
+document.querySelectorAll("input");
+document.getSelection();
+document.location;
+document.title;
+document.cookie;
+document.querySelector(".o3j99.n1xJcf.Ne6nSd").value;
+document.querySelector(".o3j99.n1xJcf.Ne6nSd").innerText;
+document.querySelector(".o3j99.n1xJcf.Ne6nSd").innerHTML;
