5949b6ca6669adc64409326dced8f54b2a65cf04
WitheredAria <55595867+WitheredAria@users.noreply.github.com>
2026-03-20 20:34:38 +0000

    upload files

---
+++ Especialización/codekata_1.js
@@ -0,0 +1,37 @@
+// Objeto tipo Array
+
+/* let namedCollection = ["Aria", "Beatriz", "Laura", "Amelia"];
+console.log(namedCollection);
+console.log(namedCollection[1]); */
+
+// Objeto tipo Set
+
+/* let enumu = new Set([1, 7, true, "Hola"]);
+//console.log(enumu.has(76));
+enumu.add("Adiós");
+console.log(enumu) */
+
+// Objeto tipo Map
+
+/* let mapeo = new Map([
+    ["1", "Aria"],
+    [2, "Alumno"]
+]);
+mapeo.set(1, "WitheredAria");
+console.log(mapeo.get("1")); */
+
+// Objeto tipo Object
+
+/* let objeto = {
+    name: "Aria",
+    surname: "Hdez. Negrín"
+};
+
+console.log(objeto.surname); */
+
+// Objeto tipo Date
+
+let currentDate = new Date();
+console.log(currentDate);
+let secondCurrentDate = new Date();
+console.log(secondCurrentDate);
---
+++ Especialización/codekata_2.js
@@ -0,0 +1,27 @@
+// Operadores aritméticos
+
+/* let numberOne = 17;
+let numberTwo = 33;
+//let result = numberOne + numberTwo;
+//let result = numberTwo - numberOne;
+//let result = numberTwo * numberOne;
+//let result = numberTwo / numberOne;
+let result = numberOne % 2;
+if (result == 0) {
+    console.log("Es par");
+} else if (result !== 0) {
+    console.log("Es impar");
+}; */
+
+/* let count = 0;
+console.log(++count);
+console.log(--count); */
+
+/* let exponent = 3;
+let numberSample = 7;
+console.log(numberSample ** exponent); */
+
+// Operadores de texto
+
+let myText = "Texto de ejemplo";
+console.log(myText + ". Con un final de ejemplo." + 1234);
