37cc577319c0c8c5c8ca04c36ae3548f85331d4e
WitheredAria <55595867+WitheredAria@users.noreply.github.com>
2026-04-10 10:55:18 +0100

    sync

---
+++ Fundamentos JS/Fundamentos.js
@@ -0,0 +1,33 @@
+let firstName = "Aria"
+let lastName = "Hernández"
+const fullName = `${firstName} ${lastName}`
+
+//Evitar hacer uso de "var" ya que puede dar lugar a errores.
+//Es preferible usar "let" para definir variables.
+
+"use strict";
+
+let student_from = "OpenWebinars"
+var course = "JavaScript for begginers";
+let year = 2023;
+
+console.log(`Im student from ${student_from} enrolled in ${coure} in ${year}`);
+
+//Es importante cerrar las líneas con ;
+
+"use strict";
+
+//var
+//let
+//function
+//if
+//else
+//try
+//catch
+//for
+//while
+
+//Palabras reservadas por el lenguaje
+
+//Siempre se debe escribir el código en inglés.
+
